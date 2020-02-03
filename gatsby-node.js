e535416062381a8119db01aee808211196aa05ca
Mariusz Roliński <mariusz@mondoreale.pl>
2020-02-03 23:55:02 +0100

    Add project aliases

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,13 @@ exports.onCreateWebpackConfig = ({ actions }) => {
                 '$hooks': path.join(__dirname, 'src/hooks'),
                 '$shared': path.join(__dirname, 'src/components/shared'),
                 '$utils': path.join(__dirname, 'src/utils'),
+                // Let's have shortcuts to all the projects.
+                '$cobalt': path.join(__dirname, 'src/components/cobalt'),
+                '$golem': path.join(__dirname, 'src/components/golem'),
+                '$hive': path.join(__dirname, 'src/components/hive'),
+                '$huxtaburger': path.join(__dirname, 'src/components/huxtaburger'),
+                '$streamr': path.join(__dirname, 'src/components/streamr'),
+                '$vizor': path.join(__dirname, 'src/components/vizor'),
                 // We want both streamr-client and pub site to use the same
                 // instance of `styled-components`.
                 'styled-components': require.resolve('styled-components'),
