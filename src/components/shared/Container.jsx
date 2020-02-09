02bd4326a0c9794063fb237b012aba3f84baf89d
Mariusz Roliński <mariusz@mondoreale.pl>
2020-02-09 23:47:36 +0100

    Store screen breakpoint in one place - $utils/css

--- src/components/shared/Container.jsx
+++ src/components/shared/Container.jsx
@@ -1,22 +1,23 @@
 import styled from 'styled-components'
+import { SM, MD, LG, XL } from '$utils/css'
 
 const Container = styled.div`
     margin: 0 auto;
     padding: 0 32px;
 
-    @media (min-width: 544px) {
+    @media (min-width: ${SM}px) {
         max-width: 540px
     }
 
-    @media (min-width: 768px) {
+    @media (min-width: ${MD}px) {
         max-width: 720px;
     }
-    
-    @media (min-width: 992px) {
+
+    @media (min-width: ${LG}px) {
         max-width: 865px;
     }
-    
-    @media (min-width: 1200px) {
+
+    @media (min-width: ${XL}px) {
         max-width: 900px;
     }
 `
---
+++ src/utils/css.js
@@ -0,0 +1,7 @@
+export const SM = 544
+
+export const MD = 768
+
+export const LG = 992
+
+export const XL = 1200
