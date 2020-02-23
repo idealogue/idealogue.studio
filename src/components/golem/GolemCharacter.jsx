d89f45a17e6cc49dd7d116dcc3f789d8a77e5cbe
Mariusz Roliński <mariusz@mondoreale.pl>
2020-02-23 20:05:54 +0100

    Style Golem clip

--- src/components/golem/GolemCharacter.jsx
+++ src/components/golem/GolemCharacter.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
 import Video from '$shared/Video'
+import Caption from '$shared/Caption'
+import Container from '$shared/Container'
 
-const GolemCharacter = () => (
-    <Video id="golemCharacter" loop autoPlay playsInline muted />
+const CaptionWrapper = styled.div`
+    position: relative;
+
+    ${Caption} {
+        position: absolute;
+        top: -128px;
+        width: 100%;
+    }
+`
+
+const UnstyledGolemCharacter = (props) => (
+    <div {...props}>
+        <Video id="golemCharacter" loop autoPlay playsInline muted />
+        <Container>
+            <CaptionWrapper>
+                <Caption>
+                    Loop animation of the Brass Golem character
+                </Caption>
+            </CaptionWrapper>
+        </Container>
+    </div>
 )
+
+const GolemCharacter = styled(UnstyledGolemCharacter)`
+    video {
+        display: block;
+        width: 100%;
+    }
+`
 
 export default GolemCharacter
