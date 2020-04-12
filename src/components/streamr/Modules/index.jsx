2639bf638e187eecdfb95afb7b7c908804a2040e
Mariusz Roliński <mariusz@mondoreale.pl>
2020-04-12 23:21:43 +0200

    Add a caption to modules

--- src/components/streamr/Modules/index.jsx
+++ src/components/streamr/Modules/index.jsx
@@ -10,6 +10,7 @@ import Scheduler from './Scheduler'
 import Search from './Search'
 import Stream from './Stream'
 import Table from './Table'
+import CaptionedContainer from '$shared/CaptionedContainer'
 
 const Row = styled.div`
     align-items: stretch;
@@ -31,74 +32,78 @@ const Cell = styled.div`
 `
 
 const UnstyledModules = (props) => (
-    <div {...props}>
-        <Row>
-            <Cell>
-                <Chart width="560" title="Chart Module" />
-            </Cell>
-            <Cell>
-                <Map width="368" title="Helsinki Map" />
-            </Cell>
-            <Cell>
-                <Table width="296" />
-            </Cell>
-        </Row>
-        <Row>
-            <Cell>
-                <Row>
-                    <Cell>
-                        <Scheduler width="224" />
-                    </Cell>
-                </Row>
-                <Row>
-                    <Cell>
-                        <Comment width="224" title="Note">
-                            Quickly test out ideas, clean up noisy data, and visualise almost
-                            anything in minutes.
-                        </Comment>
-                    </Cell>
-                </Row>
-            </Cell>
-            <Cell>
-                <Row>
-                    <Cell>
-                        <Comment width="272" title="Note">
-                            Streamr Core editor provides a comprehensiveset of modules to cover
-                            common use cases for visualisation, analysis and prototyping.
-                        </Comment>
-                    </Cell>
-                </Row>
-                <Row>
-                    <Cell>
-                        <Stream width="272" />
-                    </Cell>
-                </Row>
-            </Cell>
-            <Cell>
-                <Search width="248" height="341" />
-            </Cell>
-            <Cell>
-                <Row>
-                    <Cell>
-                        <Button width="208" />
-                    </Cell>
-                </Row>
-                <Row>
-                    <Cell>
-                        <Email width="208" />
-                    </Cell>
-                </Row>
-            </Cell>
-            <Cell>
-                <EthereumCall width="224" title="Ethereum Call" />
-            </Cell>
-        </Row>
-    </div>
+    <CaptionedContainer {...props} caption="Some of the modules used in the Core canvas editor">
+        <div>
+            <Row>
+                <Cell>
+                    <Chart width="560" title="Chart Module" />
+                </Cell>
+                <Cell>
+                    <Map width="368" title="Helsinki Map" />
+                </Cell>
+                <Cell>
+                    <Table width="296" />
+                </Cell>
+            </Row>
+            <Row>
+                <Cell>
+                    <Row>
+                        <Cell>
+                            <Scheduler width="224" />
+                        </Cell>
+                    </Row>
+                    <Row>
+                        <Cell>
+                            <Comment width="224" title="Note">
+                                Quickly test out ideas, clean up noisy data, and visualise almost
+                                anything in minutes.
+                            </Comment>
+                        </Cell>
+                    </Row>
+                </Cell>
+                <Cell>
+                    <Row>
+                        <Cell>
+                            <Comment width="272" title="Note">
+                                Streamr Core editor provides a comprehensiveset of modules to cover
+                                common use cases for visualisation, analysis and prototyping.
+                            </Comment>
+                        </Cell>
+                    </Row>
+                    <Row>
+                        <Cell>
+                            <Stream width="272" />
+                        </Cell>
+                    </Row>
+                </Cell>
+                <Cell>
+                    <Search width="248" height="341" />
+                </Cell>
+                <Cell>
+                    <Row>
+                        <Cell>
+                            <Button width="208" />
+                        </Cell>
+                    </Row>
+                    <Row>
+                        <Cell>
+                            <Email width="208" />
+                        </Cell>
+                    </Row>
+                </Cell>
+                <Cell>
+                    <EthereumCall width="224" title="Ethereum Call" />
+                </Cell>
+            </Row>
+        </div>
+    </CaptionedContainer>
 )
 
 const Modules = styled(UnstyledModules)`
-    margin: 0 auto;
-    width: 1272px;
+    > div {
+        margin: 0 auto;
+        width: 1272px;
+    }
 `
 
 export default Modules
