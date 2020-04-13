17d17d9926de019984cb058bb0a51d5d9b32ce64
Mariusz Roliński <mariusz@mondoreale.pl>
2020-04-13 23:21:50 +0200

    Sort head tags (meta go first)

---
+++ gatsby-ssr.js
@@ -0,0 +1,18 @@
+exports.onPreRenderHTML = ({ pathname, getHeadComponents, replaceHeadComponents }) => {
+    const headComponents = getHeadComponents()
+
+    const ref = [...headComponents]
+
+    headComponents.sort((a, b) => {
+        switch (true) {
+            case a.type === 'meta' && b.type !== 'meta':
+                return -1
+            case a.type !== 'meta' && b.type === 'meta':
+                return 1
+            default:
+                return ref.indexOf(a) - ref.indexOf(b)
+        }
+    })
+
+    replaceHeadComponents(headComponents)
+}
