1f5cf7761befd1dd0d229ed938c6590f3109e1d3
Mariusz Roliński <mariusz@mondoreale.pl>
2020-07-12 15:29:15 +0200

    [Vizor] Add section links

--- src/pages/vizor.jsx
+++ src/pages/vizor.jsx
@@ -20,6 +20,7 @@ import ProductLaunch360 from '$vizor/ProductLaunch360'
 import CaptionedContainer from '$shared/CaptionedContainer'
 import Spacer from '$shared/Spacer'
 import ScrollIndicator from '$shared/ScrollIndicator'
+import Topic from '../components/shared/Topic'
 
 const LayoutTheme = {
     backgroundColor: '#ffffff',
@@ -51,114 +52,119 @@ const VizorPage = () => (
                 </Hero>
             </Section>
             <ScrollIndicator.Target />
-            <Section theme={NavyTheme} head tail>
-                <SectionHeader no={1}>
-                    From Engi.works to Vizor
-                </SectionHeader>
-                <Container>
-                    <p>
-                        The app we saw in mid-2015, then known as Engi, was very rough, but powerful. It had its own WebGL-based realtime 3D graphics engine, a ton of shaders and effects, the ability to output to stereo VR views, and a deep visual programming interface. We renamed it Vizor and created a new brand style for it.
-                    </p>
-                    <Spacer head tail>
-                        <FluidImage src={Image.GRID} alt="" />
-                    </Spacer>
-                </Container>
-                <SectionHeader no={2}>
-                    Visual Programming
-                </SectionHeader>
-                <Container>
-                    <p>
-                        One of our first tasks was to clean up the programming UI. It could do almost anything, but was a confusing mess of tiny icons, unclear connections and hard to fathom structure. We introduced a colour scheme for patch types, accordions, clearer connections, and a consistent structure.
-                    </p>
-                    <Spacer head tail>
-                        <Modules />
-                    </Spacer>
-                </Container>
-                <SectionHeader no={3}>
-                    The Patches Editor
-                </SectionHeader>
-                <Container>
-                    <p>
-                        The visual programming editor, now renamed Patches and open-source, is a fast and fluid webGL-based 3D editor with switchable cameras and single-click to publish. We introduced a unique circular grid centred around the user, for easy VR composing, as well as bold, highly visible icons for HMD and lights. Try it out here.
-                    </p>
-                    <Spacer head>
-                        <BrowserFrame shadow dark>
-                            <Video id="vizor" loop autoPlay playsInline muted />
-                        </BrowserFrame>
-                    </Spacer>
-                </Container>
-            </Section>
-            <Section theme={MidnightBlueTheme} head tail>
-                <SectionHeader no={4}>
-                    The Player
-                </SectionHeader>
-                <Container>
-                    <p>
-                        Content on Vizor ranges from 360 photos to immersive stories, infographics and visual experiments. The player was designed for simplicity across 2D and VR views on desktops and mobile.
-                    </p>
-                </Container>
-                <Spacer head>
-                    <FluidImage src={Image.PHONE_FRAMES} alt="" fill />
-                </Spacer>
-            </Section>
-            <Section theme={NavyTheme} head tail>
-                <SectionHeader no={5}>
-                    User Pages
-                </SectionHeader>
-                <Container>
-                    <p>
-                        With a fully web-based file system, users needed to be able to have public and private views of their projects, along with drafts and public projects to keep things tidy. We prototyped and created this from the ground up, as it didn’t exist in the original prototype.
-                    </p>
-                    <Spacer head>
-                        <BrowserFrame shadow dark>
-                            <FluidImage src={Image.PLATFORM} alt="" fill />
-                        </BrowserFrame>
-                    </Spacer>
-                </Container>
-            </Section>
-            <Section theme={DarkBlueTheme} head tail>
-                <SectionHeader no={6}>
-                    Icons and Assets
-                </SectionHeader>
-                <Container>
-                    <p>
-                        Due to the need to keep polygon counts and file sizes down for a reasonable page load time, we worked with illustrator Jeremy Edelblut to develop a playful, low-poly style to use across 3D icons for the editor, as well as library assets for users to build with.
-                    </p>
-                </Container>
-                <Spacer head>
-                    <Icons />
-                </Spacer>
-            </Section>
-            <Section theme={NavyTheme} head>
-                <SectionHeader no={7}>
-                    Vizor 360
-                </SectionHeader>
-                <Container>
-                    <p>
-                        The Patches editor is powerful, but seeing strong user interest in 360 media led to building a new, user-friendly app for telling stories and making tours with 360 images. Built from scratch in React, Vizor 360 is fast, light and easy to use. Try it out here.
-                    </p>
-                    <Spacer head>
-                        <BrowserFrame shadow dark>
-                            <FluidImage src={Image.EDITOR_0} alt="" fill />
-                        </BrowserFrame>
-                    </Spacer>
-                </Container>
-                <Spacer head tail>
-                    <Ateneum />
-                </Spacer>
-                <Spacer tail>
-                    <CaptionedContainer caption="The Vizor 360 product launch page">
-                        <Container>
+            <Topic title="From Engi.works to Vizor">
+                <Section theme={NavyTheme} head>
+                    <SectionHeader no={1} />
+                    <Container>
+                        <p>
+                            The app we saw in mid-2015, then known as Engi, was very rough, but powerful. It had its own WebGL-based realtime 3D graphics engine, a ton of shaders and effects, the ability to output to stereo VR views, and a deep visual programming interface. We renamed it Vizor and created a new brand style for it.
+                        </p>
+                        <Spacer head>
+                            <FluidImage src={Image.GRID} alt="" />
+                        </Spacer>
+                    </Container>
+                </Section>
+            </Topic>
+            <Topic title="Visual Programming">
+                <Section theme={NavyTheme} head>
+                    <SectionHeader no={2} />
+                    <Container>
+                        <p>
+                            One of our first tasks was to clean up the programming UI. It could do almost anything, but was a confusing mess of tiny icons, unclear connections and hard to fathom structure. We introduced a colour scheme for patch types, accordions, clearer connections, and a consistent structure.
+                        </p>
+                        <Spacer head>
+                            <Modules />
+                        </Spacer>
+                    </Container>
+                </Section>
+            </Topic>
+            <Topic title="The Patches Editor">
+                <Section theme={NavyTheme} head tail>
+                    <SectionHeader no={3} />
+                    <Container>
+                        <p>
+                            The visual programming editor, now renamed Patches and open-source, is a fast and fluid webGL-based 3D editor with switchable cameras and single-click to publish. We introduced a unique circular grid centred around the user, for easy VR composing, as well as bold, highly visible icons for HMD and lights. Try it out here.
+                        </p>
+                        <Spacer head>
                             <BrowserFrame shadow dark>
-                                <FluidImage src={Image.EDITOR_1} alt="" fill />
+                                <Video id="vizor" loop autoPlay playsInline muted />
                             </BrowserFrame>
-                        </Container>
-                    </CaptionedContainer>
-                </Spacer>
-                <ProductLaunch360 />
-                <Spacer head tail>
+                        </Spacer>
+                    </Container>
+                </Section>
+            </Topic>
+            <Topic title="The Player">
+                <Section theme={MidnightBlueTheme} head tail>
+                    <SectionHeader no={4} />
+                    <Container>
+                        <p>
+                            Content on Vizor ranges from 360 photos to immersive stories, infographics and visual experiments. The player was designed for simplicity across 2D and VR views on desktops and mobile.
+                        </p>
+                    </Container>
+                    <Spacer head>
+                        <FluidImage src={Image.PHONE_FRAMES} alt="" fill />
+                    </Spacer>
+                </Section>
+            </Topic>
+            <Topic title="User Pages">
+                <Section theme={NavyTheme} head tail>
+                    <SectionHeader no={5} />
+                    <Container>
+                        <p>
+                            With a fully web-based file system, users needed to be able to have public and private views of their projects, along with drafts and public projects to keep things tidy. We prototyped and created this from the ground up, as it didn’t exist in the original prototype.
+                        </p>
+                        <Spacer head>
+                            <BrowserFrame shadow dark>
+                                <FluidImage src={Image.PLATFORM} alt="" fill />
+                            </BrowserFrame>
+                        </Spacer>
+                    </Container>
+                </Section>
+            </Topic>
+            <Topic title="Icons and Assets">
+                <Section theme={DarkBlueTheme} head tail>
+                    <SectionHeader no={6} />
+                    <Container>
+                        <p>
+                            Due to the need to keep polygon counts and file sizes down for a reasonable page load time, we worked with illustrator Jeremy Edelblut to develop a playful, low-poly style to use across 3D icons for the editor, as well as library assets for users to build with.
+                        </p>
+                    </Container>
+                    <Spacer head>
+                        <Icons />
+                    </Spacer>
+                </Section>
+            </Topic>
+            <Topic title="Vizor 360">
+                <Section theme={NavyTheme} head>
+                    <SectionHeader no={7} />
+                    <Container>
+                        <p>
+                            The Patches editor is powerful, but seeing strong user interest in 360 media led to building a new, user-friendly app for telling stories and making tours with 360 images. Built from scratch in React, Vizor 360 is fast, light and easy to use. Try it out here.
+                        </p>
+                        <Spacer head>
+                            <BrowserFrame shadow dark>
+                                <FluidImage src={Image.EDITOR_0} alt="" fill />
+                            </BrowserFrame>
+                        </Spacer>
+                    </Container>
+                    <Spacer head tail>
+                        <Ateneum />
+                    </Spacer>
+                    <Spacer tail>
+                        <CaptionedContainer caption="The Vizor 360 product launch page">
+                            <Container>
+                                <BrowserFrame shadow dark>
+                                    <FluidImage src={Image.EDITOR_1} alt="" fill />
+                                </BrowserFrame>
+                            </Container>
+                        </CaptionedContainer>
+                    </Spacer>
+                    <ProductLaunch360 />
+                </Section>
+            </Topic>
+            <Topic title="Credits">
+                <Section theme={NavyTheme} head tail>
                     <SectionHeader no={8}>
-                        Credits
                         <Bullet />
                         <Link as="a" href="https://vizor.io/" target="_blank" rel="noopener noreferrer">
                             Visit Vizor Patches
@@ -189,8 +195,8 @@ const VizorPage = () => (
                         <hr />
                     </Container>
                     <NextProject />
-                </Spacer>
-            </Section>
+                </Section>
+            </Topic>
         </Layout>
     </Project>
 )
