9edc1f35c1525ee3617cd89570b4367a5e1daee7
Mariusz Roliński <mariusz@mondoreale.pl>
2021-02-02 11:36:49 +0100

    Reindent

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,36 @@
 const siteUrl = 'https://idealogue.mariusz.io'
 
 module.exports = {
-  siteMetadata: {
-    siteUrl,
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        siteUrl,
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-cname',
-  ],
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: `${__dirname}/src/images`,
+            },
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
+        // {
+        //   resolve: `gatsby-plugin-manifest`,
+        //   options: {
+        //     name: `gatsby-starter-default`,
+        //     short_name: `starter`,
+        //     start_url: `/`,
+        //     background_color: `#663399`,
+        //     theme_color: `#663399`,
+        //     display: `minimal-ui`,
+        //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        //   },
+        // },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+        'gatsby-plugin-styled-components',
+        'gatsby-plugin-cname',
+    ],
 }
