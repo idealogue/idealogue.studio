2c79c36cccea2875fc18ec0a497cf52e85c235df
Mariusz Roliński <mariusz@mondoreale.pl>
2021-02-16 22:21:02 +0100

    Responsive hero

--- src/components/shared/Hero.jsx
+++ src/components/shared/Hero.jsx
@@ -4,20 +4,21 @@ import { useProject } from '$shared/Project'
 import Container from '$shared/Container'
 import HeroImage from '$shared/HeroImage'
 import ScrollIndicator from '$shared/ScrollIndicator'
+import { SM, MD, LG, XL } from '$utils/css'
+
+const Inner = styled.div`
+    align-items: center;
+    display: flex;
+    height: 100%;
+    text-align: center;
+`
 
 const UnstyledHero = ({ children, ...props }) => {
     const { name } = useProject()
 
     return (
         <div {...props}>
-            <div
-                css={`
-                    align-items: center;
-                    display: flex;
-                    height: 100%;
-                    text-align: center;
-                `}
-            >
+            <Inner>
                 <Container>
                     <h1>
                         {name}
@@ -25,7 +26,7 @@ const UnstyledHero = ({ children, ...props }) => {
                     {children}
                     <HeroImage />
                 </Container>
-            </div>
+            </Inner>
             <ScrollIndicator />
         </div>
     )
@@ -37,7 +38,8 @@ const Hero = styled(UnstyledHero)`
 
     h1 {
         font-family: 'Publico Text';
-        font-size: 140px;
+        font-size: 40px;
+        letter-spacing: -0.005em;
         line-height: 1;
         margin: 0;
         padding-top: 0.5em;
@@ -46,12 +48,35 @@ const Hero = styled(UnstyledHero)`
     && p {
         font-family: 'Lab Grotesque';
         font-size: 20px;
+        font-weight: 200;
         line-height: 30px;
-        margin: 48px 0;
+        margin: 48px auto;
     }
 
     img {
         margin: 0 auto;
+    }
+
+    @media (min-width: ${MD}px) {
+        h1 {
+            font-size: 88px;
+        }
+    }
+
+    @media (min-width: ${LG}px) {
+        h1 {
+            font-size: 115px;
+        }
+    }
+
+    @media (min-width: ${XL}px) {
+        h1 {
+            font-size: 140px;
+        }
+
+        && p {
+            max-width: 24em;
+        }
     }
 `
 
