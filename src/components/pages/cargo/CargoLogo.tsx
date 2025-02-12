3467af085d1ef6b039e218b5521fc4b725c2e56f
Mariusz Roliński <mariusz@mondoreale.pl>
2025-02-12 23:39:37 +0100

    Cargo: Add the missing poster

--- src/components/pages/cargo/CargoLogo.tsx
+++ src/components/pages/cargo/CargoLogo.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import Poster from './assets/logo_poster.jpg'
 
 export function CargoLogo() {
     return (
-        <CargoLogoRoot loop playsInline muted autoPlay>
+        <CargoLogoRoot loop playsInline muted autoPlay poster={Poster}>
             <source src="/videos/cargo/logo_animated.mp4" />
         </CargoLogoRoot>
     )
@@ -12,4 +13,6 @@ export function CargoLogo() {
 const CargoLogoRoot = styled.video`
     mix-blend-mode: lighten;
     width: clamp(14.5rem, 50vw, 29.75rem);
+    height: auto;
+    aspect-ratio: 1;
 `
