b40c8c7519a9aaffceee8f2e9bfebd586001ac06
Mariusz Roliński <mariusz@mondoreale.pl>
2025-02-13 02:07:46 +0100

    Deui: Fix social poster

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -5,7 +5,7 @@ const projects: Record<ProjectName, Project> = {
     deui: {
         name: 'deui',
         displayName: 'Deui',
-        posterUrl: 'https://idealogue.studio/images/Deui-share.png',
+        posterUrl: 'https://idealogue.studio/images/Idealogue-share.png',
         desc: 'Deui Case Study',
     },
     cargo: {
