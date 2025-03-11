fa13ad8f95ff9df833a8736cf35786e784393808
Mariusz Roliński <mariusz@mondoreale.pl>
2025-03-11 02:02:34 +0100

    Tweak line breaks
    I make certain word pairs stick together.

--- src/components/pages/deui/index.tsx
+++ src/components/pages/deui/index.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react'
 import styled, { css } from 'styled-components'
-import { ThemeToggle } from '~/components/pages/deui/ThemeToggle'
 import { ProjectHeroContent, ProjectImage, ProjectName } from '~/components/Project'
 import { ProjectPage } from '~/components/ProjectPage'
 import { Section } from '~/components/Section'
+import { ThemeToggle } from '~/components/pages/deui/ThemeToggle'
 import { Container } from '~/components/shared/Container'
 import { Layout } from '~/components/shared/Layout'
 import NextProject from '~/components/shared/NextProject'
 import { ScrollIndicator, ScrollIndicatorTarget } from '~/components/shared/ScrollIndicator'
 import { SectionHeader } from '~/components/shared/SectionHeader'
 import { Topic } from '~/components/shared/Topic'
-import { font, LG, MD, TABLET } from '~/utils/css'
+import { LG, MD, TABLET, font } from '~/utils/css'
 import Cup from './assets/cup.png'
 import Cup2x from './assets/cup@2x.png'
 import MachinePicture from './assets/machine.jpg'
@@ -53,17 +53,17 @@ export function DeuiPage() {
                         <Container>
                             <p>
                                 Deui is a replacement for the app that ships with Decent Espresso
-                                coffee machines. I followed Decent for years as I loved their aim of
-                                making great software-controlled espresso, and bought one when I
-                                could finally afford it. Unfortunately, what I discovered was poorly
-                                written, hobbyist-level software with little care or understanding
-                                of typography, interaction design or modern web standards. And it
-                                only ran on Android tablets.
+                                coffee machines. I&nbsp;followed Decent for years as I loved their
+                                aim of making great software-controlled espresso, and bought one
+                                when I could finally afford it. Unfortunately, what I discovered was
+                                poorly written, hobbyist-level software with little care or
+                                understanding of typography, interaction design or modern web
+                                standards. And it only&nbsp;ran on Android tablets.
                             </p>
                             <p>
                                 So I teamed up with Mariusz to make a replacement, mainly because I
-                                couldn&apos;t stand to use such unhappy software on such a generally
-                                nice espresso machine.
+                                couldn&apos;t stand to use such unhappy software on such&nbsp;a
+                                generally nice espresso machine.
                             </p>
                             <p>
                                 Our goals were for something responsive and multi-client, so I could
@@ -71,8 +71,8 @@ export function DeuiPage() {
                                 the machine from bed on lazy mornings. Simplified features were ok,
                                 no need for charts but I did want easy to read metrics, easy to
                                 select profiles, and a water level gauge. And also dark & light
-                                modes, to match the black or white only options that the machines
-                                come in.
+                                modes, to match the black or white only options that&nbsp;the
+                                machines come in.
                             </p>
                         </Container>
                     </Section>
@@ -147,10 +147,11 @@ export function DeuiPage() {
                         <SectionHeader no={2} />
                         <Container>
                             <p>
-                                The app is still a work in progress. We aim to get the server
-                                component running on Raspberry Pi so I can just put it in a cupboard
-                                & forget about it, but at the moment it is still a Node app on my
-                                MacBook. So making coffee requires some NPM terminal commands 🤓💥
+                                The app is still a work in progress. We aim&nbsp;to&nbsp;get the
+                                server component running on&nbsp;Raspberry Pi so I can just put it
+                                in a cupboard & forget about it, but at the moment it is still a
+                                Node app on my MacBook. So making coffee requires some&nbsp;NPM
+                                terminal commands 🤓💥
                             </p>
                             <p>
                                 Bluetooth scale connection for stop at weight, and a few more
