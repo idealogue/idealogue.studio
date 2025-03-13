d6b67d4e7ce79a94fb11e49e6bb6c60322132399
Mariusz Roliński <mariusz@mondoreale.pl>
2025-03-13 23:33:08 +0100

    Move

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,65 +1,9 @@
-import * as React from 'react'
-import { css } from 'styled-components'
-import { ALL as Image } from '~/components/pages/team/Image'
-import { Collection, Person } from '~/components/pages/team/Person'
-import { Section } from '~/components/Section'
-import { FluidImage } from '~/components/shared/FluidImage'
-import { Layout } from '~/components/shared/Layout'
+import type { HeadFC } from 'gatsby'
+import React from 'react'
 import { Seo } from '../components/Seo'
 
-export default function TeamPage() {
-    return (
-        <Layout rootCss={layoutCss}>
-            <Section actConsecutive spacerProps={{ head: true, tail: true }} css={defaultCss}>
-                <Collection>
-                    <Person
-                        img={
-                            <FluidImage
-                                src={Image.MATT}
-                                srcSet={`${Image.MATT_2X} 2x`}
-                                alt="Matt"
-                            />
-                        }
-                        name="Matt Innes"
-                        role="Designer / Creative Director"
-                    >
-                        Matt has worked as a designer and creative director in Australia and Japan
-                        for more than 10 years. He holds a degree in Communication Design from
-                        Swinburne School of Design, Melbourne. He has led design for Web3 projects
-                        including Hive Wallet, Counterparty, Golem and Streamr. When not hiking,
-                        he&apos;ll usually be backcountry snowboarding in Japan.
-                    </Person>
-                    <Person
-                        img={
-                            <FluidImage
-                                src={Image.SAORI}
-                                srcSet={`${Image.SAORI_2X} 2x`}
-                                alt="Saori"
-                            />
-                        }
-                        name="Saori Kajiwara"
-                        role="UI/UX Designer"
-                    >
-                        A native of Shizuoka, Japan, Saori holds a&nbsp;degree in Communication
-                        Design from Swinburne Design School, Melbourne. She&nbsp;has worked as an
-                        illustrator and designer in Melbourne and Tokyo. She is most comfortable
-                        building animations and&nbsp;prototypes with her toolkit of Sketch, Figma,
-                        Invision and After Effects.
-                    </Person>
-                </Collection>
-            </Section>
-        </Layout>
-    )
-}
+export { TeamPage as default } from '$team'
 
-export const Head = () => {
+export const Head: HeadFC = () => {
     return <Seo />
 }
-
-const layoutCss = css`
-    --Layout_Background: #ffffff;
-`
-
-const defaultCss = css`
-    --MenuToggle_Color: #06042a;
-`
